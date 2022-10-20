423f2c635ef803b87dc46235579bc86a4835431e
Azara-Nya <77995029+Azara-Nya@users.noreply.github.com>
2022-10-20 21:55:19 +0100

    Added Locking Functionallity

--- Assets/Scripts/ResourceGiver.cs
+++ Assets/Scripts/ResourceGiver.cs
@@ -5,12 +5,24 @@ using UnityEngine;
 public class ResourceGiver : MonoBehaviour
 {
     [SerializeField] private int Resource; //Wood:0, Stone:1
+    [SerializeField] private int PaymentResource;
+    [SerializeField] private int cost;
+    [SerializeField] private bool Unlocked;
     [SerializeField] ResourceManger ReM;
 
     public void Add_Resource()
     {
+        if(Unlocked)
+        {
         //Wait Specific Amount of Time
         ReM.Resources[Resource] += 1;
-        Debug.Log(ReM.Resources[Resource]);
+        Debug.Log($"{ReM.Resources[Resource]}, {Resource}");
+        }
+
+        if(!Unlocked && ReM.Resources[PaymentResource] >= cost)
+        {
+                Unlocked = !Unlocked;
+                ReM.Resources[PaymentResource] -= cost;
+        }    
     }
 }
