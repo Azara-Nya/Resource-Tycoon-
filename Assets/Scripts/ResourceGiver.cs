2fca51aeae9fe252ac29220107c7e76792af5a93
Azara-Nya <77995029+Azara-Nya@users.noreply.github.com>
2023-01-19 19:18:58 +0000

    Added Resource Givers Only Give Resource On Break
    + Removed unlocking functionality

--- Assets/Scripts/ResourceGiver.cs
+++ Assets/Scripts/ResourceGiver.cs
@@ -4,40 +4,40 @@ using UnityEngine;
 
 public class ResourceGiver : MonoBehaviour
 {
-    [SerializeField] private string Resource="Wood"; 
-    [SerializeField] private int Resource_Number=0;
-    [SerializeField] private string PaymentResource;
-    [SerializeField] private int PaymentResource_Number=0;
-    [SerializeField] private int cost;
-    [SerializeField] private bool Unlocked;
+    [SerializeField] private int Charges=2;
+    [SerializeField] private int dropAmount=1;
     [SerializeField] private float Resource_Time=1f;
     [SerializeField] private GameObject ResourcePrefab;
+    [SerializeField] private Transform dropPoint;
     private bool canGive;
-    private int times;
     public bool currently_adding;
-    [SerializeField] ResourceManger ReM;
-    [SerializeField] Inventory Inven;
+    private int times;
+    private int uses;
 
-    public void Add_Resource()
-    {
-        if(!currently_adding)
-        {
-        StartCoroutine(Resource_Adder());
-        }
-    }
 
     void Update()
     {
-        if(canGive && Input.GetKeyDown(KeyCode.E))
+        if(uses < Charges)
         {
-            Add_Resource();
+            if(canGive && Input.GetKeyDown(KeyCode.E))
+                {
+                  Add_Resource();
+                }
+        }
+        else
+        {
+            for(int i=0;i<dropAmount;i++)
+                {
+                 Instantiate(ResourcePrefab, dropPoint.position, dropPoint.rotation);
+                }
+            Destroy(gameObject);
         } 
     }
     void OnTriggerEnter2D(Collider2D other)
     {
         if(other.CompareTag("Player"))
         {          
-            canGive = true;
+        canGive = true;
         }
     }
 
@@ -50,48 +50,28 @@ public class ResourceGiver : MonoBehaviour
         }
     }
 
-    IEnumerator Resource_Adder()
+    public void Add_Resource()
     {
-        
+        if(!currently_adding)
         {
-        if(Unlocked)
-        {
+            StartCoroutine(Resource_Adder());
+        }
+    }
+
+    IEnumerator Resource_Adder()
+    {    
+        {  
             currently_adding = !currently_adding;
         yield return new WaitForSeconds(Resource_Time);
 
         if(canGive)
         {
-            for(int i = 0; i < Inven.Inv.Length; i++)
-            {
-                if(Inven.Inv[i]=="Nothing")
-               {
-                   Inven.Inv[i] = Resource;
-                   Inven.InvContent[i] = ResourcePrefab;
-                   ReM.Resources[Resource_Number] += 1;
-                    break;
-               }
-          }
+            uses += 1;
+        }
              currently_adding = !currently_adding;
         }
-        }
-
-        if(!Unlocked && ReM.Resources[PaymentResource_Number] >= cost)
-        {
-                Unlocked = !Unlocked;
-                ReM.Resources[PaymentResource_Number] -= cost;
-
-                for(int i = 0; i < Inven.Inv.Length; i++)
-                {         
-                    if(Inven.Inv[i] == PaymentResource && times != cost)
-                    {
-                        times++;
-                        Inven.Inv[i] = "Nothing";
-                        Inven.InvContent[i] = null;
-                        ReM.Resources[Resource_Number]-=1;
-                    }
-                }    
-                times=0;
-        }
+    
          
     }
-}}
+}
+
